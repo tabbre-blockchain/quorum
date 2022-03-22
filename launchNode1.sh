52dc8afffdadbce4c3024b9ec552fc55bb7416a2
Tabbre Admin <30662298+tabbre-blockchain@users.noreply.github.com>
2022-03-22 11:24:33 +0000

    Add files via upload

---
+++ launchNode1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+NETWORK_ID=$(cat genesis.json | tr -d '\r' | grep chainId | awk -F " " '{print $2}' | awk -F "," '{print $1}')
+
+DATADIR=" --datadir 1 "
+ARGS="--nodiscover --verbosity 5 --istanbul.blockperiod 5 --networkid $NETWORK_ID --syncmode full --mine --minerthreads 1 --emitcheckpoints " 
+PORT="--port 23001 "
+geth $DATADIR $ARGS $PORT
